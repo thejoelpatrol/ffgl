040ba995fd143b3fd170024fd124b0a31854371a
Joel Cretan <jcretan@gmail.com>
2026-02-14 16:12:31 -0500

    add RGBHSV

---
+++ build/osx/hsv/hsv.cpp
@@ -0,0 +1,194 @@
+#include "hsv.h"
+using namespace ffglex;
+using namespace ffglqs;
+
+static CFFGLPluginInfo PluginInfo(
+    PluginFactory< RGBHSV >,// Create method
+    "RE0888",                      // Plugin unique ID of maximum length 4.
+    "RGB-HSV",            // Plugin name
+    2,                           // API major version number
+    1,                           // API minor version number
+    1,                           // Plugin major version number
+    0,                           // Plugin minor version number
+    FF_EFFECT,                   // Plugin type
+    "rgb-hsv, but fast now",  // Plugin description
+    "mostly the Resolume FFGL Example"      // About
+);
+
+static const char _vertexShaderCode[] = R"(#version 410 core
+uniform vec2 MaxUV;
+
+layout( location = 0 ) in vec4 vPosition;
+layout( location = 1 ) in vec2 vUV;
+
+out vec2 uv;
+
+void main()
+{
+    gl_Position = vPosition;
+    uv = vUV * MaxUV;
+}
+)";
+
+static const char _fragmentShaderCode[] = R"(#version 410 core
+uniform sampler2D InputTexture;
+uniform int Bits;
+
+in vec2 uv;
+
+out vec4 fragColor;
+
+int HSBtoRGB(float hue, float saturation, float brightness) {
+    int r = 0, g = 0, b = 0;
+    if (saturation == 0) {
+        r = g = b = int(brightness * 255.0 + 0.5);
+    } else {
+        int hue2 = int(hue);
+        //float h = (hue - float(hue2)) * 6.0;
+        float h = hue * 6.0;
+        int h_floor = int(h);
+        float f = h - float(h_floor);
+        float p = brightness * (1.0 - saturation);
+        float q = brightness * (1.0 - saturation * f);
+        float t = brightness * (1.0 - (saturation * (1.0 - f)));
+        switch (h_floor) {
+        case 0:
+            r = int(brightness * 255.0 + 0.5);
+            g = int(t * 255.0 + 0.5);
+            b = int(p * 255.0 + 0.5);
+            break;
+        case 1:
+            r = int(q * 255.0 + 0.5);
+            g = int(brightness * 255.0 + 0.5);
+            b = int(p * 255.0 + 0.5);
+            break;
+        case 2:
+            r = int(p * 255.0 + 0.5);
+            g = int(brightness * 255.0 + 0.5);
+            b = int(t * 255.0 + 0.5);
+            break;
+        case 3:
+            r = int(p * 255.0 + 0.5);
+            g = int(q * 255.0 + 0.5);
+            b = int(brightness * 255.0 + 0.5);
+            break;
+        case 4:
+            r = int(t * 255.0 + 0.5);
+            g = int(p * 255.0 + 0.5);
+            b = int(brightness * 255.0 + 0.5);
+            break;
+        case 5:
+            r = int(brightness * 255.0 + 0.5);
+            g = int(p * 255.0 + 0.5);
+            b = int(q * 255.0 + 0.5);
+            break;
+        }
+    }
+    return 0xff000000 | (r << 16) | (g << 8) | (b << 0);
+}
+
+void main()
+{
+    vec4 color = texture( InputTexture, uv );
+    //The InputTexture contains premultiplied colors, so we need to unpremultiply first to apply our effect on straight colors.
+    if( color.a > 0.0 )
+        color.rgb /= color.a;
+
+    //color.rgb += Brightness * 2. - 1.;
+
+    // ------------ BEGIN JOEL CODE ------------
+    //float rf = color.rgb.r * 255;
+    //float gf = color.rgb.g * 255;
+    //float bf = color.rgb.b * 255;
+
+    int hsv = HSBtoRGB(color.rgb.r, color.rgb.g, color.rgb.b);
+    int r = (0x00FF0000 & hsv) >> 16;
+    int g = (0x0000FF00 & hsv) >> 8;
+    int b = 0x000000FF & hsv;
+
+    float rf = float(r) / 255.0;
+    float gf = float(g) / 255.0;
+    float bf = float(b) / 255.0;
+    color.rgb.r = rf;
+    color.rgb.g = gf;
+    color.rgb.b = bf;
+    
+    // ------------ END JOEL CODE ------------
+
+
+    //The plugin has to output premultiplied colors, this is how we're premultiplying our straight color while also
+    //ensuring we aren't going out of the LDR the video engine is working in.
+    color.rgb = clamp( color.rgb * color.a, vec3( 0.0 ), vec3( color.a ) );
+    fragColor = color;
+}
+)";
+
+RGBHSV::RGBHSV()
+{
+    // Input properties
+    SetMinInputs( 1 );
+    SetMaxInputs( 1 );
+
+
+    FFGLLog::LogToHost( "Created RGBHSV effect" );
+}
+RGBHSV::~RGBHSV()
+{
+}
+
+FFResult RGBHSV::InitGL( const FFGLViewportStruct* vp )
+{
+    if( !shader.Compile( _vertexShaderCode, _fragmentShaderCode ) )
+    {
+        FFGLLog::LogToHost( "##### failed to compile RGBHSV!" );
+        DeInitGL();
+        return FF_FAIL;
+    }
+    if( !quad.Initialise() )
+    {
+        DeInitGL();
+        return FF_FAIL;
+    }
+
+    //Use base-class init as success result so that it retains the viewport.
+    return CFFGLPlugin::InitGL( vp );
+}
+FFResult RGBHSV::ProcessOpenGL( ProcessOpenGLStruct* pGL )
+{
+    if( pGL->numInputTextures < 1 )
+        return FF_FAIL;
+
+    if( pGL->inputTextures[ 0 ] == NULL )
+        return FF_FAIL;
+
+    //FFGL requires us to leave the context in a default state on return, so use this scoped binding to help us do that.
+    ScopedShaderBinding shaderBinding( shader.GetGLID() );
+    //The shader's sampler is always bound to sampler index 0 so that's where we need to bind the texture.
+    //Again, we're using the scoped bindings to help us keep the context in a default state.
+    ScopedSamplerActivation activateSampler( 0 );
+    Scoped2DTextureBinding textureBinding( pGL->inputTextures[ 0 ]->Handle );
+
+    shader.Set( "inputTexture", 0 );
+
+    //The input texture's dimension might change each frame and so might the content area.
+    //We're adopting the texture's maxUV using a uniform because that way we dont have to update our vertex buffer each frame.
+    FFGLTexCoords maxCoords = GetMaxGLTexCoords( *pGL->inputTextures[ 0 ] );
+    shader.Set( "MaxUV", maxCoords.s, maxCoords.t );
+
+    //This takes care of sending all the parameter that the plugin registered to the shader.
+    SendParams( shader );
+
+    quad.Draw();
+    
+    
+    return FF_SUCCESS;
+}
+
+
+FFResult RGBHSV::DeInitGL()
+{
+    shader.FreeGLResources();
+    quad.Release();
+
+    return FF_SUCCESS;
+}
